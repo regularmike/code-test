6e7759cc2c8f5eb560d7148b8ed8ec944ac3f974
Michael Sullivan <regularmike@gmail.com>
2020-10-20 01:22:41 +0000

    add Subscription model and migration

---
+++ database/migrations/2020_10_20_005344_create_subscriptions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                  ->constrained()
+                  ->onDelete('cascade');
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->timestamps();            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
